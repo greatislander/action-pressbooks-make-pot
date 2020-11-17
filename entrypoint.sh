6c89ef821abce54e6c9112e3316eed12643b4cf5
Ned Zimmerman <ned@bight.dev>
2020-11-16 20:38:19 -0400

    fix: restore wp pb

--- entrypoint.sh
+++ entrypoint.sh
@@ -38,8 +38,5 @@ elif [[ ! -d $PATH ]]; then
     mkdir $PATH
 fi
 
-# Generate the POT file from Blade.
-find . -iname '*.blade.php' | xargs xgettext --language=Python --add-comments=TRANSLATORS --force-po --from-code=UTF-8 --default-domain=de_DE -k__ -k_e -k_n:1,2 -k_x:1,2c -k_ex:1,2c -k_nx:4c,12 -kesc_attr__ -kesc_attr_e -kesc_attr_x:1,2c -kesc_html__ -kesc_html_e -kesc_html_x:1,2c -k_n_noop:1,2 -k_nx_noop:3c,1,2, -k__ngettext_noop:1,2 -j -o "$PATH/$TEXTDOMAIN.pot"
-
 # Generate the POT file from other resources.
-wp i18n make-pot . "$PATH/$TEXTDOMAIN.pot" --exclude="$EXCLUDES" --slug="$SLUG" --package-name="$NAME" --headers="$HEADERS" --domain="$TEXTDOMAIN" --allow-root
+wp pb make-pot . "$PATH/$TEXTDOMAIN.pot" --exclude="$EXCLUDES" --slug="$SLUG" --package-name="$NAME" --headers="$HEADERS" --domain="$TEXTDOMAIN" --allow-root
